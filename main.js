7616da52d0e0630c8fb32e4a56affc25f07ade62
razalxgpt-sudo <razalx.gpt@gmail.com>
2026-04-22 06:43:37 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 // boot scene placeholder
+window.onload = function () {
+
+const config = {
+type: Phaser.AUTO,
+width: 800,
+height: 600,
+backgroundColor: "#000000",
+scene: [BootScene, GameScene]
+};
+
+const game = new Phaser.Game(config);
+
+};
