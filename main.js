09a66dba8661c9b18c57b2b6b9f2c3becb0b663a
razalxgpt-sudo <razalx.gpt@gmail.com>
2026-04-22 07:11:27 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-window.onload = function () {
+document.addEventListener("DOMContentLoaded", function () {
 
 const config = {
-type: Phaser.AUTO,
-width: 800,
-height: 600,
-backgroundColor: "#000000",
-scene: [BootScene, GameScene]
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    backgroundColor: "#000000",
+    scene: [BootScene, GameScene]
 };
 
 new Phaser.Game(config);
 
-};
+});
