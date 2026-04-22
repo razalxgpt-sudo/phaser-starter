3c9d9f26ff1e50cd136bacc1b973db5c1d82db34
razalxgpt-sudo <razalx.gpt@gmail.com>
2026-04-22 06:50:37 +0300

    Update profile.js

--- profile.js
+++ profile.js
@@ -1,11 +1,3 @@
-class GameCore {
-    constructor(scene){
-        this.scene = scene;
-        this.scene.add.text(20,20,'MindFlow Adaptive',{
-            fontSize:'24px',
-            fill:'#ffffff'
-        });
-    }
-
-    update(){}
-}
+const Profile = {
+score: 0
+};
