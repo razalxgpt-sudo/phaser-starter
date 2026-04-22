ca09ad5bb61ae353965fc35fdc5f9382535913d3
razalxgpt-sudo <razalx.gpt@gmail.com>
2026-04-22 12:27:25 +0300

    Update GameScene.js

--- GameScene.js
+++ GameScene.js
@@ -7,11 +7,18 @@ create() {
 
 this.createAdaptiveBackground();
 
-/* codul tău existent pentru joc rămâne aici */
+this.nodes = [];
+this.lines = [];
+this.selectedNode = null;
+
+this.createNodes();
+
+this.input.on("pointerdown", this.onPointerDown, this);
+this.input.on("pointerup", this.onPointerUp, this);
 
 }
 
-/* ---------------- BACKGROUND SYSTEM ---------------- */
+/* ---------- BACKGROUND ---------- */
 
 createAdaptiveBackground() {
 
@@ -19,26 +26,22 @@ const palettes = {
 
 very_easy: [
 [0x1e3a8a, 0x60a5fa],
-[0x065f46, 0x34d399],
-[0x7c3aed, 0xa78bfa]
+[0x065f46, 0x34d399]
 ],
 
 easy: [
 [0x0f172a, 0x334155],
-[0x111827, 0x374151],
-[0x0b132b, 0x1c2541]
+[0x111827, 0x374151]
 ],
 
 normal: [
 [0x020617, 0x0f172a],
-[0x030712, 0x111827],
-[0x020617, 0x020617]
+[0x030712, 0x111827]
 ],
 
 relaxed: [
 [0x1c1917, 0x44403c],
-[0x1f2933, 0x4b5563],
-[0x1e293b, 0x475569]
+[0x1f2933, 0x4b5563]
 ]
 
 };
@@ -77,4 +80,80 @@ graphics.fillRect(0, i, width, 1);
 }
 
 }
+
+/* ---------- GAME LOGIC ---------- */
+
+createNodes() {
+
+const width = this.scale.width;
+const height = this.scale.height;
+
+for (let i = 0; i < 5; i++) {
+
+const x = Phaser.Math.Between(100, width - 100);
+const y = Phaser.Math.Between(100, height - 100);
+
+const node = this.add.circle(x, y, 15, 0xffffff)
+.setInteractive();
+
+this.nodes.push(node);
+
+}
+
+}
+
+onPointerDown(pointer) {
+
+this.nodes.forEach(node => {
+
+const dist = Phaser.Math.Distance.Between(
+pointer.x,
+pointer.y,
+node.x,
+node.y
+);
+
+if (dist < 20) {
+this.selectedNode = node;
+}
+
+});
+
+}
+
+onPointerUp(pointer) {
+
+if (!this.selectedNode) return;
+
+this.nodes.forEach(node => {
+
+const dist = Phaser.Math.Distance.Between(
+pointer.x,
+pointer.y,
+node.x,
+node.y
+);
+
+if (dist < 20 && node !== this.selectedNode) {
+
+const line = this.add.line(
+0,0,
+this.selectedNode.x,
+this.selectedNode.y,
+node.x,
+node.y,
+0xffffff
+);
+
+line.setOrigin(0,0);
+this.lines.push(line);
+
+}
+
+});
+
+this.selectedNode = null;
+
+}
+
 }
