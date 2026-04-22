ca44666dbef453f49ded331e8d0a7a91cdd86dbe
razalxgpt-sudo <razalx.gpt@gmail.com>
2026-04-22 07:25:08 +0300

    Update BootScene.js

--- BootScene.js
+++ BootScene.js
@@ -1,23 +1,14 @@
 class BootScene extends Phaser.Scene {
 
-    constructor() {
-        super("BootScene");
-    }
+constructor() {
+    super("BootScene");
+}
 
-    create() {
+preload() {
+}
 
-        const w = this.cameras.main.width;
-        const h = this.cameras.main.height;
+create() {
+    this.scene.start("GameScene");
+}
 
-        this.add.text(w / 2, h / 2, "MindFlow Running", {
-            fontFamily: "Courier New",
-            fontSize: "28px",
-            color: "#ffffff",
-            align: "center"
-        }).setOrigin(0.5);
-
-        this.time.delayedCall(1000, () => {
-            this.scene.start("GameScene");
-        });
-    }
 }
