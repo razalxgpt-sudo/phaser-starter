56e6d94118b7e961826737973bf1fcfaa79be67c
razalxgpt-sudo <razalx.gpt@gmail.com>
2026-04-22 06:50:59 +0300

    Update save.js

--- save.js
+++ save.js
@@ -1,17 +1,5 @@
-class PlayerProfile {
-    constructor(){
-        this.score = 0;
-        this.level = 1;
-    }
-
-    save(){
-        localStorage.setItem('mindflow_profile', JSON.stringify(this));
-    }
-
-    load(){
-        const data = localStorage.getItem('mindflow_profile');
-        if(data){
-            Object.assign(this, JSON.parse(data));
-        }
-    }
+const Save = {
+save() {
+console.log("saved");
 }
+};
